3dcdaeb7a142df69a81bed4bf484ca5c78263ec0
proLmpa <kheeyeoul@gmail.com>
2025-10-16 15:43:52 +0900

    Root project files commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,23 @@
 plugins {
-    kotlin("jvm") version "2.2.0"
+    // 서브 프로젝트에서 Kotlin/JVM을 사용하도록 설정
+    id("org.jetbrains.kotlin.jvm") version "1.9.23" apply false
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "com.example"
+    version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(24)
+    dependencies {
+        val implementation by configurations
+        // 모든 서브 프로젝트가 Kotlin 표준 라이브러리를 사용하도록 설정
+        implementation(kotlin("stdlib"))
+    }
 }
